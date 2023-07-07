b4268e687aa348130fc9845f92ec761c09b5a589
Shivanshu Yadav <shivanshu.yadav04@gmail.com>
2023-07-08 02:49:27 +0530

    Ticket listing Page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Entry } from './page/entry/entry.page';
 import {DefaultLayout} from './component/layout/DefaultLayout';
 import { Dashboard } from "./page/dashboard/Dashboard.page";
 import { AddTicket } from "./page/new-ticket/AddTicket.page";
+import { TicketLists } from "./component/ticket-list/TicketLists.page";
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
           {/*<Dashboard/>*/}
-          <AddTicket/>
+          {/*<AddTicket/>*/}
+          <TicketLists/>
       </DefaultLayout>
       {/*<Entry/>*/}
     </div>
